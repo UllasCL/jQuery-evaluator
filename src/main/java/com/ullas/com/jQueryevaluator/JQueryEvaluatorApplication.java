fbf9d0ca2a53f8ac87a34d32c82ae33efa388d60
UllasCL <ullascl.1si15cs127@gmail.com>
2023-06-01 17:32:12 +0530

    project setup

---
+++ src/main/java/com/ullas/com/jQueryevaluator/JQueryEvaluatorApplication.java
@@ -0,0 +1,13 @@
+package com.ullas.com.jQueryevaluator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JQueryEvaluatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JQueryEvaluatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ullas/com/jQueryevaluator/JQueryEvaluatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ullas.com.jQueryevaluator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JQueryEvaluatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
